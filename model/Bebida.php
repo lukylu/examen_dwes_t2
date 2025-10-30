93381b3f6e91ae7e544440de29ef23455ce5c8cd
Lucas <lucasfdeziglesias24@gmail.com>
2025-10-30 12:35:28 +0100

    bebida.php finalizado

--- model/Bebida.php
+++ model/Bebida.php
@@ -1,3 +1,14 @@
 <?php
 // Clase Bebida que hereda de Artículo
+require_once "articulo.php";
 
+class Bebida extends Articulo {
+    public $tamaño;
+    public $temperatura;
+
+    public function __construct($nombre, $precio, $disponibilidad, $categoria, $tamaño, $temperatura) {
+        parent::__construct($nombre, $precio, $disponibilidad, $categoria);
+        $this->tamaño = $tamaño;
+        $this->temperatura = $temperatura;
+    }
+}
